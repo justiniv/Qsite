1c8483ca1ef1505ab5dad36ad74f20ca04d0e548
Justin IV <justinvalenz@gmail.com>
2017-07-12 11:14:46 -0400

    functions edit

--- includes/functions.php
+++ includes/functions.php
@@ -10,25 +10,7 @@
 			exit;
 		}
 	}
-	/*
-	function __autoload($class_name) 
-	{
-		$class_name 	= strtolower($class_name);
-		$path 		=  $class_name . ".php";
-		
-		if(file_exists($path))
-		{
-			require_once($path);
-			
-			echo "Class file found. <br />";
-		}
-		else 
-		{
-			die("The file ". $class_name .".php could not be found.");
-		}
-		
-	}
-	*/
+	
 	
 	spl_autoload_register(function($class_name) 
 	{
